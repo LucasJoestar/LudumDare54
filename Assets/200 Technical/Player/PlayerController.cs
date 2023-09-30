a26559af948d8ae1f7bb5a0318c989c2d77d9bf3
Lucas Guibert <lucas@ludogram.io>
2023-09-30 16:09:15 +0200

    Quick commit

---
+++ Assets/200 Technical/Player/PlayerController.cs
@@ -0,0 +1,12 @@
+using EnhancedEditor;
+using EnhancedFramework.Core;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace LudumDare54
+{
+    public class PlayerController : EnhancedSingleton<PlayerController> {
+        
+    }
+}
