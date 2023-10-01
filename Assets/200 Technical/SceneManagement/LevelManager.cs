0eeab1181ee7a8d2642e50a6ffd97680c6ed7643
Ysnir <nayplatypusmith@live.fr>
2023-10-01 11:58:55 +0200

    adaptative grid

--- Assets/200 Technical/SceneManagement/LevelManager.cs
+++ Assets/200 Technical/SceneManagement/LevelManager.cs
@@ -9,13 +9,11 @@ namespace LudumDare54
     public class LevelManager : EnhancedSingleton<LevelManager>
     {
         [SerializeField]
-        public int weight;
+        public int width;
         [SerializeField]
         public int height;
         [SerializeField]
         public List<int> Grid;
-        [SerializeField]
-        public List<Vector2> GridBlockMap;
         [SerializeField]
         public GameObject Empty;
         [SerializeField]
@@ -23,13 +21,11 @@ namespace LudumDare54
 
 
 
-        [Button(ActivationMode.Play, SuperColor.Green, IsDrawnOnTop=false)]
         public void InitLevel(int w, int h)
         {
-            this.weight = w;
+            this.width = w;
             this.height = h;
             this.Grid = new List<int>();
-            this.GridBlockMap = new List<Vector2>();
             int cpt = 0;
             for (int i = 0; i < w; i++) {
                 for (int j = 0; j < h; j++)
@@ -41,11 +37,18 @@ namespace LudumDare54
             }
         }
 
+        [Button(ActivationMode.Play, SuperColor.Green, IsDrawnOnTop = false)]
+        public void InitLevel()
+        {
+            this.height = 2 * (int)Camera.main.orthographicSize;
+            this.width = height * (int)Camera.main.aspect;
+            InitLevel(this.width, this.height);
+        }
+
         [Button(ActivationMode.Play, SuperColor.Blue, IsDrawnOnTop = false)]
         public void PlaceBlock(Vector2 position)
         {
             Instantiate(Block, new Vector3(0.5f * position.x, 0.5f * position.y, 0), Quaternion.identity);
-            this.GridBlockMap.Add(position);
         }
 
         [Button(ActivationMode.Play, SuperColor.Red, IsDrawnOnTop = false)]
@@ -63,12 +66,12 @@ namespace LudumDare54
 
         public bool IsOutOfGrid(Vector2 position)
         {
-            return position.x < 0 || position.y < 0 || position.x > this.weight-1 || position.y > this.height-1;
+            return position.x < 0 || position.y < 0 || position.x > this.width-1 || position.y > this.height-1;
         }
 
         public int ToGridCoordinate(Vector2 position)
         {
-            return (int)position.y*this.weight + (int)position.x;
+            return (int)position.y*this.width + (int)position.x;
         }
     }
 }
